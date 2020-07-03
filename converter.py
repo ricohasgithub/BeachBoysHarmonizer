30f4e976cd28f7634cfb7c82784f3655048128d2
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-07-03 16:27:30 -0400

    Implemented Audio Split Saving
     - Added proper sub-directory creation and population with wav, mel, and array files

--- converter.py
+++ converter.py
@@ -3,8 +3,8 @@
 Converter.py
 
 This file contains the method convert_wav_file that converts from .wav files to mel spectrograms via librosa's
-Short-Term Fourier Transformations (STFT) graphing algorithms. Used to preprocess sound files to images for
-Convolutional Neural Networks.
+Short-Term Fourier Transformations (STFT) graphing algorithms. Each audio file is first split into segments of 5 seconds each
+Used to preprocess sound files to images for Convolutional Neural Networks.
 
 '''
 
@@ -18,11 +18,17 @@ import matplotlib.pyplot as plt
 import librosa
 import librosa.display
 
-# This method takes in the path to the assets folder containing the .wav audio file to be processed. It transforms the audio file into a spectrogram (and associated pandas pd array) and saved under the filename as a png
+# This method takes in the path to the assets folder containing the .wav audio file to be processed. It transforms the audio file into a spectrogram (and associated pandas pd array) and saved under the filename as a png in splices of 5 seconds each
 def convert_wav_file(assets_path, filename):
 
     # Assets_path is only the path to the assets; apply pathing to the source folder and the save folder
     audio_path = assets_path + "/source/" + filename + ".wav"
+
+    # Output path to put the generated directories
+    output_path = assets_path + "/output/" + filename
+
+    # Create the new output path
+    os.makedirs(output_path)
 
     # Buffer the currently loaded audio file
     y, sr = librosa.load(audio_path)
@@ -42,20 +48,35 @@ def convert_wav_file(assets_path, filename):
             buffer = splits_total - splits_written
 
         block = y[splits_written : (splits_written + buffer)]
-        out_filename = "split_" + str(splits_counter) + "_" + filename
 
-        # Write 2 second segment
-        librosa.output.write_wav(out_filename, block, sr)
+        # Create a new output path for the current split
+        split_path = output_path + str(splits_counter) + filename
+
+        # Create the new split audio directory
+        os.makedirs(split_path)
+
+        # Output paths for the truncated wav file, mel spectrogram and associated array
+        wav_path = split_path + ".wav"
+        mel_path = split_path + "/" + filename + ".png"
+        pd_arr_path = split_path + "/" + filename + ".csv"
+
+        # Write 5 second segment
+        librosa.output.write_wav(wav_path, block, sr)
+
+        # Create and populate the sub-directory
+        convert_split_file(wav_path, mel_path, pd_arr_path)
+
+        # Increment the current buffer
         splits_counter += 1
         splits_written += buffer
 
-    # Output paths for the mel spectrogram and associated array
-    output_path = assets_path + "/output/" + filename
-    mel_path = output_path + "/" + filename + ".png"
-    pd_arr_path = output_path + "/" + filename + ".csv"
 
-    # Create a new output path
-    os.makedirs(output_path)
+    print("A wild success!")
+
+def convert_split_file (wav_path, mel_path, pd_arr_path):
+
+    # Load the new spliced audio file
+    y, sr = librosa.load(wav_path)
 
     # Construct and display a mel-scaled power (energy-squared) spectrogram
     S = librosa.feature.melspectrogram(y, sr=sr, n_mels=128)
@@ -87,5 +108,3 @@ def convert_wav_file(assets_path, filename):
 
     # Save the spectrogram in the path folder for the spectrograms as .png's
     plt.savefig(mel_path)
-
-    print("A wild success!")
